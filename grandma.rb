5ef6d0b52ae346c9457db642ce106b317df18eb5
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-16 21:36:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase != phrase.uppercase
+  if phrase != phrase.upcase
     return "HUH?! SPEAK UP< SONNY!"
-  elsif phrase == phrase.uppercase
+  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
