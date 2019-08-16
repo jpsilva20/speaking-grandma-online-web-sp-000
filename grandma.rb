9b5fb5a1943112661362f420c817fdcf994a6654
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-16 21:36:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
-    return "HUH?! SPEAK UP< SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
